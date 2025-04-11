11a83eda4e57d4a6773d9ac74bb1f2bc7ecff2df
Aman Kumar <amankumarmaurya93@gmail.com>
2025-04-11 21:18:24 +0530

    Java database connectivity project, how to access data from database, I have successfully accessed my data from my database, Postgresql, Also I can Fetch, Put, Update, delete data from Intellij Idea using java

--- src/Demo.java
+++ src/Demo.java
@@ -5,16 +5,17 @@ public class Demo {
     public static void main(String args[]) throws SQLException {
 
 
-        String sql = "select address from students where id = 2";
+        String sql = "select name from russian where id = 3";
 
         String url = "jdbc:postgresql://127.0.0.1:5432/product";
         String username = "postgres";
-        String password = "AWS4sdlish";
+        String password = "0000";
         Connection con = DriverManager.getConnection(url, username, password);
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(sql);
         rs.next();
         String name = rs.getString(1);
         System.out.println(name);
+        System.out.println("Query Executed Successfully");
     }
 }
